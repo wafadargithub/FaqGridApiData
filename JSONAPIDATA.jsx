11fdb805a3384eb63be2af8d5b61e36df3811f7b
wafadar hussain <124126541+wafadargithub@users.noreply.github.com>
2025-02-17 16:00:42 -0800

    Add files via upload
    api and token save data

---
+++ JSONAPIDATA.jsx
@@ -0,0 +1,13 @@
+
+// used to the GetAllFAQ data from user include the jquery database Table 
+// {
+//     "API_URL": "https://uat-api.askaii.co.uk/api/web/getallfaqs",
+//     "TOKEN": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InBhcGFqYW5pMTIzNEBnbWFpbC5jb20iLCJuYmYiOjE3MzI1MjY0NjksImV4cCI6MTczMjUzMDA2OSwiaWF0IjoxNzMyNTI2NDY5fQ.aapZxuNOmdVQwmGN1sWJWJ64V4cSeIZGpDfwOZOohG8"
+//   }
+
+
+// used to the api hit from authorization  login form api
+// {
+//     "apiBaseUrl": "https://uat-api.askaii.co.uk/api/user",
+//      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InBhcGFqYW5pMTIzNEBnbWFpbC5jb20iLCJuYmYiOjE3MzI1MjY0NjksImV4cCI6MTczMjUzMDA2OSwiaWF0IjoxNzMyNTI2NDY5fQ.aapZxuNOmdVQwmGN1sWJWJ64V4cSeIZGpDfwOZOohG8"
+//    }
